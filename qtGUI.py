ce7c969c3da795d6bb9e2bd44c5caa061e94f412
MengxunLi <mengxunli@whu.edu.cn>
2020-02-28 16:52:34 -0700

    switching to new branch using qt creater

--- qtGUI.py
+++ qtGUI.py
@@ -1,4 +1,6 @@
-from PyQt5.QtWidgets import QMainWindow, QWidget, QAction, QFileDialog
+# from PyQt5.QtWidgets import QMainWindow, QWidget, QAction, QFileDialog, QComboBox, QFrame, Qsplitter, QVBoxLayout
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
 from version import __VERSION__, __DESCRIPTION__
 from dicomFileReader import FolderLoader
 from pathlib import Path
@@ -13,17 +15,19 @@ class MainWindow(QMainWindow):
         self.showMaximized()
         self.setWindowTitle("LabelSys "+__VERSION__)
         self.menu_bar = self.menuBar()
-        self.__initMenu()
-        if self.args.dev: self.__loadPatients()
+        self._initMenu()
+        self.frame = QWidget(self)
+        self.setCentralWidget(self.frame)
+        if self.args.dev: self._loadPatients()
 
-    def __initMenu(self):
+    def _initMenu(self):
         menu_file = self.menu_bar.addMenu('File')
         act_imp = QAction("Import", self)
-        act_imp.triggered.connect(self.__loadPatients)
+        act_imp.triggered.connect(self._loadPatients)
         menu_file.addAction(act_imp)
 
-    def __loadPatients(self):
-        """Load patients folder and display in the mainwindow"""
+    def _loadPatients(self):
+        """Load patients folder, and call _initPanel() to initialize main panel""" 
         if self.args.dev: 
             fname = self.args.file
         else: fname = QFileDialog.getExistingDirectory(self, "Select Directory")
@@ -31,13 +35,34 @@ class MainWindow(QMainWindow):
             return 1
         file_path = Path(fname)
         self.fl = FolderLoader(file_path)
+        self._initPanel()
         return 0
 
-    def __initImageUI(self):
+    def _initPanel(self):
+        """Init the whole panel, will be called on loading the patients""" 
+        grid = QGridLayout()
+        self.frame.setLayout(grid)
+        grid.setSpacing(10)
+
+        self.combo_series = QComboBox(self)
+        self.__updateComboSeries()
+        self.combo_series.currentTextChanged.connect(self.slotChangeComboSeries)
+
+        grid.addWidget(self.combo_series, 0,0)
+
+    def _initImageUI(self):
         """Put image on to main window"""
         pass
 
-    def __askChooseSeries(self):
-        """ Prompt user to choose a series to display
-        Will be called each time change patient"""
-        pass
+    def slotChangeComboSeries(self, entry):
+        """Triggered when self.combo_series change the entry, will change the
+        image showing"""
+        print(entry)
+        
+
+    def __updateComboSeries(self):
+        """Update the series combobox with image series get from self.fl.curr_patient"""
+        self.combo_series.clear()
+        series = self.fl.curr_patient.getEntries()
+        self.combo_series.addItems(series)
+
