7b1d073337b6e686062566c6fde84056c7d82f42
MengxunLi <mengxunli@whu.edu.cn>
2020-02-28 17:57:39 -0700

    changed to using qt designer and put console output into the program

--- qtGUI.py
+++ qtGUI.py
@@ -1,33 +1,34 @@
 # from PyQt5.QtWidgets import QMainWindow, QWidget, QAction, QFileDialog, QComboBox, QFrame, Qsplitter, QVBoxLayout
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, pyqtSignal, QObject
+from PyQt5 import uic
 from version import __VERSION__, __DESCRIPTION__
 from dicomFileReader import FolderLoader
 from pathlib import Path
+import os,sys
+
+LOCAL_DIR = os.path.dirname(os.path.realpath(__file__))
 
 class MainWindow(QMainWindow):
     def __init__(self,args):
         super().__init__()
+        # load UI
+        ui_path = os.path.join(LOCAL_DIR, "mainWindow.ui")
+        uic.loadUi(ui_path, self)
         self.args = args
-        self.initUI()
-
-    def initUI(self):
-        self.showMaximized()
-        self.setWindowTitle("LabelSys "+__VERSION__)
-        self.menu_bar = self.menuBar()
-        self._initMenu()
-        self.frame = QWidget(self)
-        self.setCentralWidget(self.frame)
+        # if dev mode, read from cmd
         if self.args.dev: self._loadPatients()
 
-    def _initMenu(self):
-        menu_file = self.menu_bar.addMenu('File')
-        act_imp = QAction("Import", self)
-        act_imp.triggered.connect(self._loadPatients)
-        menu_file.addAction(act_imp)
+        self.showMaximized()
+        self.setWindowTitle("LabelSys "+__VERSION__)
+
+        sys.stdout = EmittingStream(textWritten = self.slotPutOnConsole)
+
+    def _initMenuAct(self):
+        self.act_imp.triggered.connect(self._loadPatients)
 
     def _loadPatients(self):
-        """Load patients folder, and call _initPanel() to initialize main panel""" 
+        """Load patients folder, and call _initPanelAct() to initialize the panel""" 
         if self.args.dev: 
             fname = self.args.file
         else: fname = QFileDialog.getExistingDirectory(self, "Select Directory")
@@ -35,20 +36,13 @@ class MainWindow(QMainWindow):
             return 1
         file_path = Path(fname)
         self.fl = FolderLoader(file_path)
-        self._initPanel()
+        self._initPanelAct()
         return 0
 
-    def _initPanel(self):
+    def _initPanelAct(self):
         """Init the whole panel, will be called on loading the patients""" 
-        grid = QGridLayout()
-        self.frame.setLayout(grid)
-        grid.setSpacing(10)
-
-        self.combo_series = QComboBox(self)
-        self.__updateComboSeries()
+        self._updateComboSeries()
         self.combo_series.currentTextChanged.connect(self.slotChangeComboSeries)
-
-        grid.addWidget(self.combo_series, 0,0)
 
     def _initImageUI(self):
         """Put image on to main window"""
@@ -59,10 +53,18 @@ class MainWindow(QMainWindow):
         image showing"""
         print(entry)
         
+    def slotPutOnConsole(self, text):
+       self.tb_console.append(text) 
 
-    def __updateComboSeries(self):
+    def _updateComboSeries(self):
         """Update the series combobox with image series get from self.fl.curr_patient"""
         self.combo_series.clear()
         series = self.fl.curr_patient.getEntries()
         self.combo_series.addItems(series)
 
+class EmittingStream(QObject):
+    """Reference: https://stackoverflow.com/questions/8356336/how-to-capture-output-of-pythons-interpreter-and-show-in-a-text-widget"""
+    textWritten = pyqtSignal(str)
+
+    def write(self, text):
+        self.textWritten.emit(str(text))
