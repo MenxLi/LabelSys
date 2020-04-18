ffe2c98a9510457e3af8344d6523c940a271c6c7
Monsoon <limengxun45@outlook.com>
2020-04-18 01:21:59 -0600

    small change

--- mainWindowGUI_hipEffusion.py
+++ mainWindowGUI_hipEffusion.py
@@ -21,9 +21,12 @@ class MainWindow_HE(MainWindow):
 
     def otsuContour(self):
         mask = super()._getSingleMask(self.slice_id, self.curr_lbl)
+        if mask == None:
+            return 1
         im = self.imgs[self.slice_id]
         pixels = np.ma.masked_array(im, 1-mask).compressed() # labeled pixels
         t = F.otsuThresh(pixels)
         _ , thresh = cv.threshold(im, t, im.max(), cv.THRESH_BINARY)
         self.imgs[self.slice_id] = thresh*mask + self.imgs[self.slice_id]*(1-mask)
         super()._updateImg()
+        return 0
