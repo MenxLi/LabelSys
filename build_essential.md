96785aa8ebda0b83d7408e6e9a39102d3e289a5c
Monsoon <limengxun45@outlook.com>
2020-04-21 20:31:17 -0600

    version rule change, add build files for pyinstaller

---
+++ build_essential.md
@@ -0,0 +1,13 @@
+# Data/Library Files necessary for pyinstaller to build excutable
+
+[Pyinstaller-Spec](http://pyinstaller.readthedocs.io/en/stable/spec-files.html#adding-data-files)
+
+```Python
+data = [
+( "ui/*.ui", "./ui" ),
+( "utils/*.dll", "./utils" ),
+( "utils/*.so", "./utils" ),
+( "help.html", "./" ),
+( "Pics", "./Pics" )
+]
+```
--- version.py
+++ version.py
@@ -7,10 +7,10 @@ __maintainer__ = "Mengxun Li"
 __email__ = "mengxunli@whu.edu.cn | mengxun1@ualberta.ca"
 
 __VERSIONS__ =[ \
-        ["1.X", "Implemented with Tkinter"],\
-        ["2.0-beta", "Re-write the whole program using PyQt5 and vtk, under develpment"],\
-        ["2.0.0", "For MRI labeling - disc, condyle and eminence"],
-        ["2.0.1", "Fix curve initialization crash bug, add individual curve initialization step, \
-         add image/video loading support"]
+        ["0.X", "Implemented with Tkinter"],
+        ["1.0-alpha", "Re-write the whole program using PyQt5 and vtk, under develpment"],
+        ["1.0.0", "For MRI labeling - disc, condyle and eminence"],
+        ["1.0.1", "Fix curve initialization crash bug; Add individual curve initialization step; Add image/video loading support"],
+        ["1.1.0", "Speed up loading and exporting process; Change data storing header file rule to include configration; Add compare widget; Add labeling panel preview; Change version naming rule"]
         ]
 __version__, __description__ = __VERSIONS__[-1]
