752074204ac44f89886fb4fccd320e50f3bd9416
limengxun <limengxun45@outlook.com>
2022-09-24 16:44:30 +0800

    fix: better merge overlapped masks

--- labelSys/clib/mergeMasks.c
+++ labelSys/clib/mergeMasks.c
@@ -5,7 +5,34 @@ typedef unsigned char arr_t;
 typedef unsigned char msk_t;
 typedef int coord_t;
 
+/*
+ *
+ * n_color: number of colors
+ * colors: array of pointers to single colors, length = n_color
+ * dest: destination, array of 3 elements
+ *
+ * */
+void fuseColor(int n_color, arr_t ** colors, arr_t * dest){
+    // single color situation
+    if (n_color == 1){
+        for (int c = 0; c<3; c++){
+            dest[c] = colors[0][c];
+        }
+        return;
+    }
 
+    int sum_color[3] = {0, 0, 0};
+    // traverse colors
+    for (int i = 0; i<n_color; i++){
+        for (int c = 0; c<3; c++){
+            sum_color[c] += colors[i][c];
+        }
+    }
+    // set the destination
+    for (int c = 0; c<3; c++){
+        dest[c] = sum_color[c]/n_color;
+    }
+}
 
 /* 
  *
@@ -20,6 +47,11 @@ EXPORT void mergeBool2Color2D( arr_t * dest, msk_t * msks, arr_t * colors, int i
     arr_t * color;
     msk_t * msk;
 
+    // selected colors at certain pixel
+    // the selected colors will be fused to get final color on the mask
+    unsigned int n_sel_colors;
+    arr_t * sel_colors[n_msks];
+
     // traverse all pixels
     for (int row = 0; row<im_h; row++){
         for (int col = 0; col < im_w; col ++){
@@ -28,17 +60,20 @@ EXPORT void mergeBool2Color2D( arr_t * dest, msk_t * msks, arr_t * colors, int i
             dst = dest + (row*im_w + col)*3;
 
             // traverse all masks at (row, col)
+            n_sel_colors = 0;
             for (int m=0; m<n_msks; m++){
                 // the mask pointer
                 msk = msks + m*(im_h*im_w) + row*im_w + col;
                 if (*msk == 1){
                     color = colors + 3*m;
-                    dst[0] = color[0];
-                    dst[1] = color[1];
-                    dst[2] = color[2];
-                    break;
+                    sel_colors[n_sel_colors] = color;
+                    n_sel_colors += 1;
+                    /* dst[0] = color[0]; */
+                    /* dst[1] = color[1]; */
+                    /* dst[2] = color[2]; */
                 }
             }
+            fuseColor(n_sel_colors, sel_colors, dst);
         }
     }
 }
