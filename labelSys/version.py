68fdbd22d790dcea64cbe1825b652d31825c2881
monsoon <mengxunli@whu.edu.cn>
2022-09-25 17:59:24 +0800

    1.9.2

--- labelSys/version.py
+++ labelSys/version.py
@@ -80,5 +80,6 @@ __VERSIONS__ =[ \
         ["1.8.4", "Bug fix: crash when edit comment"],
         ["1.9.0", "Using PyQt6!"],
         ["1.9.1", "Optimize 2D color preview speed; drag and drop to open/load; extention hooks"],
+        ["1.9.2", "Jsonc as configuration file; build update; bug fixes"],
         ]
 __version__, __description__ = __VERSIONS__[-1]
